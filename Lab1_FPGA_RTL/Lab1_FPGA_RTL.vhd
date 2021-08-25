97f8a27f59542d0251cb9a4aff00493c3418ed10
VictorLMoura <victorlaperutademoura@hotmail.com>
2021-08-25 03:57:23 -0700

    feat: tutorial 1

--- Lab1_FPGA_RTL/Lab1_FPGA_RTL.vhd
+++ Lab1_FPGA_RTL/Lab1_FPGA_RTL.vhd
@@ -3,11 +3,13 @@ use IEEE.std_logic_1164.all;
 
 entity Lab1_FPGA_RTL is
     port (
-        -- Gloabals
+        -- Globals
         fpga_clk_50   : in  std_logic;        
-
+		  
         -- I/Os
-        fpga_led_pio  : out std_logic_vector(5 downto 0)
+		  button : in  std_logic;
+		  sw: in std_logic_vector(2 downto 0);
+		  fpga_led_pio  : out std_logic_vector(5 downto 0)
   );
 end entity Lab1_FPGA_RTL;
 
@@ -15,19 +17,42 @@ architecture rtl of Lab1_FPGA_RTL is
 
 -- signal
 signal blink : std_logic := '0';
+signal current_state : std_logic := '0';
+
+shared variable current_multiplier: integer range 1 to 4 := 1;
 
 begin
-
-  process(fpga_clk_50) 
+	process(sw)
+		begin
+			current_multiplier := 1;
+			if (sw(0) = '1') then
+				current_multiplier := current_multiplier + 1;
+			end if;
+			if (sw(1) = '1') then
+				current_multiplier := current_multiplier + 1;
+			end if;
+			if (sw(2) = '1') then
+				current_multiplier := current_multiplier + 1;
+			end if;
+	end process;
+			
+	process(button)
+		begin
+		if (button = '1') then
+			current_state <= not current_state;
+		end if;
+	end process;
+	
+  process(fpga_clk_50, current_state) 
       variable counter : integer range 0 to 25000000 := 0;
       begin
-        if (rising_edge(fpga_clk_50)) then
-                  if (counter < 10000000) then
-                      counter := counter + 1;
-                  else
-                      blink <= not blink;
-                      counter := 0;
-                  end if;
+        if (rising_edge(fpga_clk_50) and current_state = '1') then
+				if (counter < 25000000/current_multiplier) then
+					 counter := counter + 1;
+				else
+					 blink <= not blink;
+					 counter := 0;
+				end if;
         end if;
   end process;
 
