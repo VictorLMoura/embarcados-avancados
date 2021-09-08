6ee81742f95d198da7876348f518fb065f813c43
VictorLMoura <victorlaperutademoura@hotmail.com>
2021-09-08 14:39:26 -0700

    feat: tutorial l 2

--- Lab2_FPGA_NIOS/Lab1_FPGA_RTL.vhd
+++ Lab2_FPGA_NIOS/Lab1_FPGA_RTL.vhd
@@ -1,66 +1,31 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity Lab1_FPGA_RTL is
+entity LAB1_FPGA_RTL is
     port (
-        -- Globals
-        fpga_clk_50   : in  std_logic;        
-		  
+        -- Gloabals
+        fpga_clk_50        : in  std_logic;             -- clock.clk
+
         -- I/Os
-		  button : in  std_logic;
-		  sw: in std_logic_vector(2 downto 0);
-		  fpga_led_pio  : out std_logic_vector(5 downto 0)
+        fpga_led_pio       : out std_logic_vector(5 downto 0)
   );
-end entity Lab1_FPGA_RTL;
+end entity LAB1_FPGA_RTL;
 
-architecture rtl of Lab1_FPGA_RTL is
+architecture rtl of LAB1_FPGA_RTL is
 
--- signal
-signal blink : std_logic := '0';
-signal current_state : std_logic := '0';
-
-shared variable current_multiplier: integer range 1 to 4 := 1;
+component niosLab2 is port (
+  clk_clk       : in  std_logic                    := 'X'; -- clk
+  reset_reset_n : in  std_logic                    := 'X'; -- reset_n
+  leds_export   : out std_logic_vector(5 downto 0)         -- export
+);
+end component niosLab2;
 
 begin
-	process(sw)
-		begin
-			current_multiplier := 1;
-			if (sw(0) = '1') then
-				current_multiplier := current_multiplier + 1;
-			end if;
-			if (sw(1) = '1') then
-				current_multiplier := current_multiplier + 1;
-			end if;
-			if (sw(2) = '1') then
-				current_multiplier := current_multiplier + 1;
-			end if;
-	end process;
-			
-	process(button)
-		begin
-		if (button = '1') then
-			current_state <= not current_state;
-		end if;
-	end process;
-	
-  process(fpga_clk_50, current_state) 
-      variable counter : integer range 0 to 25000000 := 0;
-      begin
-        if (rising_edge(fpga_clk_50) and current_state = '1') then
-				if (counter < 25000000/current_multiplier) then
-					 counter := counter + 1;
-				else
-					 blink <= not blink;
-					 counter := 0;
-				end if;
-        end if;
-  end process;
 
-  fpga_led_pio(0) <= blink;
-  fpga_led_pio(1) <= blink;
-  fpga_led_pio(2) <= blink;
-  fpga_led_pio(3) <= blink;
-  fpga_led_pio(4) <= blink;
-  fpga_led_pio(5) <= blink;
+u0 : component niosLab2 port map (
+  clk_clk       => fpga_clk_50,    --  clk.clk
+  reset_reset_n => '1',            --  reset.reset_n
+  leds_export   => fpga_led_pio    --  leds.export
+);
 
 end rtl;
